8e8cc857db2d2dcdf2d8b4be7d6f67184bd673e4
IgnatSergeev <ignat.sergeev@softcom.su>
2024-09-05 16:55:52 +0300

    CI: format, build, lint, test
    Add gitignore
    
    Initialize project
    
    Add linting and nix-shell
    
    Updated linting
    
    Changed ocaml version
    
    Changed tests
    
    Formatted
    
    Changed test dependency
    
    Checking pinning
    
    Fixed test dependency
    
    Updated linting
    
    Updated linting
    
    Removed description
    
    Updated comment
    
    Updated github repo specification
    
    Updated copyright years

---
+++ lib/HelloWorld.ml
@@ -0,0 +1,5 @@
+(** Copyright 2024, MacadamiaSolver. *)
+
+(** SPDX-License-Identifier: MIT *)
+
+let getString () = "Hello world"
---
+++ lib/HelloWorld.mli
@@ -0,0 +1,5 @@
+(** Copyright 2024, MacadamiaSolver. *)
+
+(** SPDX-License-Identifier: MIT *)
+
+val getString : unit -> string
