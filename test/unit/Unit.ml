72e302d1afb924fc6c268562b778b9a1c9a2819e
TimaFrolov <tima1911@yandex.ru>
2024-09-16 18:48:23 +0300

    Format

--- test/unit/Unit.ml
+++ test/unit/Unit.ml
@@ -1,1 +1,3 @@
-let () = Alcotest.run "MacadamiaSolver" [TestParser.tests; TestNfa.tests; TestNfaCollection.tests]
+let () =
+  Alcotest.run "MacadamiaSolver"
+    [TestParser.tests; TestNfa.tests; TestNfaCollection.tests]
