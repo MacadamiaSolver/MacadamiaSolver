56f6ef741fe7e9c776affc896d4905e380b4b364
Georgiy Belyanin <belyaningeorge@ya.ru>
2024-11-24 14:08:41 +0300

    parser: support statements and new operators
    This patch extends a parser with a statements. Now it parses solver
    commands, e.g:
    * `parse` --- intended for testing the parser.
    * `help` --- intended for printing the help.
    * `eval` --- intended for proving/disproving the statement.
    
    And supports a variety of new binary operators:
    * `<->` --- iff.
    * `->` --- implies.
    * `>=`/`<=`/`!=` --- new comparison predicates.
    
    Also it adds tests with Jane-Street expect tests.

--- lib/ast.mli
+++ lib/ast.mli
@@ -1,19 +1,44 @@
 (** Copyright 2024, MacadamiaSolver. *)
 
-(** SPDX-License-Identifier: MIT *)
+(* SPDX-License-Identifier: MIT *)
 
 type varname = string
 
-type term = Var of varname | Const of int | Add of term * term
+type predname = string
+
+type term =
+  | Var of varname
+  | Const of int
+  | Add of term * term
+  | Mul of int * term
+  | Pow of int * term
 
 type formula =
-  | Equals of term * term
+  | True
+  | False
+  | Pred of predname * term list
+  | Eq of term * term
+  | Neq of term * term
+  | Lt of term * term
+  | Gt of term * term
+  | Leq of term * term
+  | Geq of term * term
   | Mnot of formula
   | Mand of formula * formula
   | Mor of formula * formula
   | Mimpl of formula * formula
-  | Exists of varname * formula
-  | Any of varname * formula
+  | Miff of formula * formula
+  | Exists of varname list * formula
+  | Any of varname list * formula
+  | Pow2 of term
+
+type stmt =
+  | Def of string * varname list * formula
+  | Eval of formula
+  | Dump of formula
+  | Parse of formula
+  | List
+  | Help
 
 val var : varname -> term
 
@@ -21,7 +46,27 @@ val const : int -> term
 
 val add : term -> term -> term
 
-val equals : term -> term -> formula
+val mul : int -> term -> term
+
+val pow : int -> term -> term
+
+val pred : predname -> term list -> formula
+
+val mtrue : unit -> formula
+
+val mfalse : unit -> formula
+
+val eq : term -> term -> formula
+
+val neq : term -> term -> formula
+
+val lt : term -> term -> formula
+
+val gt : term -> term -> formula
+
+val leq : term -> term -> formula
+
+val geq : term -> term -> formula
 
 val mnot : formula -> formula
 
@@ -31,10 +76,37 @@ val mor : formula -> formula -> formula
 
 val mimpl : formula -> formula -> formula
 
-val exists : varname -> formula -> formula
+val miff : formula -> formula -> formula
 
-val any : varname -> formula -> formula
+val exists : varname list -> formula -> formula
 
-val string_of_term : term -> string
+val any : varname list -> formula -> formula
 
-val string_of_formula : formula -> string
+val def : string -> varname list -> formula -> stmt
+
+val eval : formula -> stmt
+
+val dump : formula -> stmt
+
+val parse : formula -> stmt
+
+val list : unit -> stmt
+
+val help : unit -> stmt
+
+val pp_term : Format.formatter -> term -> unit
+
+val pp_formula : Format.formatter -> formula -> unit
+
+val quantifier_ast_exn : formula -> varname list -> formula -> formula
+
+val binconj_ast_exn : formula -> formula -> formula -> formula
+
+val fold :
+  ('acc -> formula -> 'acc) -> ('acc -> term -> 'acc) -> 'acc -> formula -> 'acc
+
+val for_some : (formula -> bool) -> (term -> bool) -> formula -> bool
+
+val for_all : (formula -> bool) -> (term -> bool) -> formula -> bool
+
+val map : (formula -> formula) -> (term -> term) -> formula -> formula
--- lib/parser.ml
+++ lib/parser.ml
@@ -4,26 +4,26 @@
 
 open Angstrom
 
-let ( << ) f g x = f (g x)
-
 let is_whitespace = function ' ' | '\t' | '\n' | '\r' -> true | _ -> false
 
 let whitespace = take_while is_whitespace
 
 let is_digit = function '0' .. '9' -> true | _ -> false
 
-let integer = take_while1 is_digit >>| (Ast.const << int_of_string)
+let const = take_while1 is_digit >>| int_of_string >>| Ast.const
 
-let is_smallchar = function 'a' .. 'z' -> true | _ -> false
+let is_idschar = function 'a' .. 'z' | '_' -> true | _ -> false
 
-let varname =
-  lift2
-    (fun a b -> String.make 1 a ^ b)
-    (satisfy is_smallchar) (take_while is_digit)
+let is_idchar = function 'a' .. 'z' | '_' | '0' .. '9' -> true | _ -> false
 
-let var = varname >>| Ast.var
+let ident =
+  let* a = satisfy is_idschar in
+  let* b = take_while is_idchar in
+  String.make 1 a ^ b |> return
 
-let sum = whitespace *> char '+' *> whitespace *> return Ast.add
+let var = ident >>| Ast.var
+
+let integer = take_while1 is_digit >>| int_of_string
 
 let parens p = char '(' *> whitespace *> p <* whitespace <* char ')'
 
@@ -31,45 +31,121 @@ let chainl1 e op =
   let rec go acc = lift2 (fun f x -> f acc x) op e >>= go <|> return acc in
   e >>= go
 
+let un_op op ast p = string op *> whitespace *> p >>| ast
+
+let bin_op op ast p =
+  let op = whitespace *> string op *> whitespace *> return ast in
+  chainl1 p op
+
+let opt q p = q p <|> p
+
+let pow term =
+  let* c = integer <* whitespace in
+  let* body = term in
+  Ast.mul c body |> return
+
+let mul term =
+  let* c = integer <* whitespace <* string "**" <* whitespace in
+  let* body = term in
+  Ast.pow c body |> return
+
 let term =
-  fix (fun _ ->
-      let aterm = integer <|> var in
-      chainl1 aterm sum )
+  fix (fun term ->
+      parens term <|> const <|> var |> opt pow |> opt mul |> bin_op "+" Ast.add )
 
-let mand = whitespace *> char '&' *> whitespace *> return Ast.mand
+let pred =
+  let* name = ident <* whitespace in
+  let* params = whitespace *> term |> many <* whitespace in
+  Ast.pred name params |> return
 
-let mor = whitespace *> char '|' *> whitespace *> return Ast.mor
+let pred_op op ast =
+  let* a = term in
+  let* _ = whitespace *> string op <* whitespace in
+  let* b = term in
+  ast a b |> return
 
-let mimpl = whitespace *> string "->" *> whitespace *> return Ast.mimpl
+let aformula =
+  pred_op "=" Ast.eq <|> pred_op "!=" Ast.neq <|> pred_op "<" Ast.lt
+  <|> pred_op ">" Ast.gt <|> pred_op "<=" Ast.leq <|> pred_op ">=" Ast.geq
+  <|> pred <?> "Expected aformula"
 
-let equals = whitespace *> char '=' *> whitespace *> return Ast.equals
+let quantifier sym ast formula =
+  let* _ = char sym in
+  let* var = ident in
+  let* formula = whitespace *> formula in
+  ast [var] formula |> return
 
 let formula =
   fix (fun formula ->
-      let equals =
-        lift3
-          (fun a _ b -> Ast.equals a b)
-          term
-          (whitespace *> char '=' *> whitespace)
-          term
+      let formula1 =
+        parens formula <|> aformula
+        |> opt (un_op "~" Ast.mnot)
+        |> bin_op "&" Ast.mand |> bin_op "|" Ast.mor |> bin_op "->" Ast.mimpl
+        |> bin_op "<->" Ast.miff
       in
-      let mnot = char '~' *> whitespace *> formula >>| Ast.mnot in
-      let exists =
-        char 'E'
-        *> lift2 (fun a b -> Ast.exists a b) varname (whitespace *> formula)
-      in
-      let any =
-        char 'A'
-        *> lift2 (fun a b -> Ast.any a b) varname (whitespace *> formula)
-      in
-      let aformula = parens formula <|> exists <|> any <|> mnot <|> equals in
-      let aformula2 = chainl1 aformula mand in
-      let aformula3 = chainl1 aformula2 mor in
-      chainl1 aformula3 mimpl )
+      quantifier 'A' Ast.any formula
+      <|> quantifier 'E' Ast.exists formula
+      <|> formula1 )
 
-let parse (str : string) : Ast.formula =
-  match parse_string ~consume:All formula str with
-    | Ok v ->
-        v
-    | Error msg ->
-        failwith msg
+let kw kw ast = string kw *> whitespace *> return ast
+
+let kw1 kw ast p1 =
+  let* _ = string kw <* whitespace in
+  let* p1 = p1 in
+  ast p1 |> return
+
+let kw2 kw ast p1 p2 =
+  let* _ = string kw <* whitespace in
+  let* p1 = p1 in
+  ast p1 p2
+
+let kw3 kw ast p1 p2 p3 =
+  let* _ = string kw <* whitespace in
+  let* p1 = p1 in
+  let* p2 = p2 in
+  let* p3 = p3 in
+  ast p1 p2 p3 |> return
+
+let def =
+  let* name = string "let" *> whitespace *> ident <* whitespace in
+  let* params = many (whitespace *> ident) in
+  let* body = whitespace *> char '=' *> whitespace *> formula in
+  Ast.def name params body |> return
+
+let stmt =
+  kw1 "eval" Ast.eval formula
+  <|> kw3 "let" Ast.def (ident <* whitespace)
+        (many (whitespace *> ident))
+        (whitespace *> char '=' *> whitespace *> formula)
+  <|> kw1 "dump" Ast.dump formula
+  <|> kw1 "parse" Ast.parse formula
+  <|> kw "list" (Ast.list ())
+  <|> kw "help" (Ast.help ())
+  <?> "Unknown statement"
+
+let parse_formula str = parse_string ~consume:Prefix formula str
+
+let parse str = parse_string ~consume:Prefix stmt str
+
+let%expect_test "parse simple formula" =
+  Format.printf "%a" Ast.pp_formula
+    (parse_formula {|Ax x = 2 | x != 2|} |> Result.get_ok);
+  [%expect {| (Ax ((x = 2) | (x != 2))) |}]
+
+let%expect_test "parse multiple quantifier formula" =
+  Format.printf "%a" Ast.pp_formula
+    (parse_formula {|ExEy z = 5x + 3y|} |> Result.get_ok);
+  [%expect {| (Ex (Ey (z = ((5 * x) + (3 * y))))) |}]
+
+let%expect_test "parse long chain" =
+  Format.printf "%a" Ast.pp_formula
+    (parse_formula {|x = 2 & y = 3 & z = 4 & a = 1 & b = 5|} |> Result.get_ok);
+  [%expect {| (((((x = 2) & (y = 3)) & (z = 4)) & (a = 1)) & (b = 5)) |}]
+
+let%expect_test "parse parens and complex priorities" =
+  Format.printf "%a" Ast.pp_formula
+    ( parse_formula
+        {|a = 1 & b = 2 & (t + 2(z + 3q) + (2d + 5w) >= 15 | (Ex Ey x + y = 15))|}
+    |> Result.get_ok );
+  [%expect
+    {| (((a = 1) & (b = 2)) & ((((t + (2 * (z + (3 * q)))) + ((2 * d) + (5 * w))) >= 15) | (Ex (Ey ((x + y) = 15))))) |}]
