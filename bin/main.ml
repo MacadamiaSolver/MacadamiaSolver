4be8eb575241c328599beef9b9088e8b629a2904
Georgiy Belyanin <belyaningeorge@ya.ru>
2024-12-04 20:15:29 +0300

    solver: add support for PrA solution procedure
    This patch adds an implementation of the basic Presburger
    Arithmetic solver. The procedure is based on a classical automata-based
    approach for proving Presburger arithmetic statements.
    
    For each atomic formula an automaton is built and then they're
    intersected/unioned corresponding to the atomic formula logical
    connectives.
    
    Now, when running the solver, the REPL is provided, supporting a variety
    of basic commands:
    * `eval` for proving/disproving statements.
    * `parse` for dumping the AST for the formula.
    * `dump` for drawing the NFA using GraphViz.
    
    Closes #12

--- bin/main.ml
+++ bin/main.ml
@@ -4,14 +4,53 @@
 
 open Lib
 
+let exec line = function
+  | Ast.Eval f -> (
+      let res = Solver.proof f in
+      match res with
+        | Ok res ->
+            Format.printf "Result: %b\n\n%!" res
+        | Error msg ->
+            Format.printf "Error: %s\n\n%!" msg )
+  | Ast.Dump f -> (
+    match Solver.dump f with
+      | Ok s ->
+          let dot_file = Format.sprintf "dumps/\"%s.dot\"" line in
+          let svg_file = Format.sprintf "dumps/\"%s.svg\"" line in
+          let oc = open_out (Format.sprintf "dumps/%s.dot" line) in
+          let command =
+            Format.sprintf "mkdir -p dumps/; dot -Tsvg %s > %s; xdg-open %s"
+              dot_file svg_file svg_file
+          in
+          Printf.fprintf oc "%s" s;
+          close_out oc;
+          Sys.command command |> ignore
+      | Error msg ->
+          Format.printf "Error: %s\n\n%!" msg )
+  | Ast.Def (name, params, formula) -> (
+    match Solver.pred name params formula with
+      | Ok () ->
+          ()
+      | Error msg ->
+          Format.printf "Error: %s\n\n%!" msg )
+  | Ast.Parse f ->
+      Format.printf "Formula AST: %a\n%!" Ast.pp_formula f
+  (* TODO: Support for optimizer. *)
+  (* Format.printf "Optimized AST: %a\n\n%!" Ast.pp_formula f *)
+  | Ast.List ->
+      Solver.list ()
+  | Ast.Help ->
+      ()
+
 let () =
-  let rec input_and_solve () =
-    let expr = read_line () in
-    let ast = Parser.parse_formula expr in
-    match ast with
-      | Ok f ->
-          input_and_solve (Format.printf "%a" Ast.pp_formula f)
-      | Error err ->
-          input_and_solve (Format.printf "Error: %s" err)
+  let rec aux () =
+    let line = read_line () in
+    let stmt = Parser.parse line in
+    match stmt with
+      | Ok stmt ->
+          exec line stmt; aux ()
+      | Error msg ->
+          Format.printf "Error: %s\n\n%!" msg;
+          aux ()
   in
-  input_and_solve ()
+  aux ()
