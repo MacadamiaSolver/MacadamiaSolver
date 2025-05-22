ffc59ad6daee922bbfb11947a329f9d128e58b7a
TimaFrolov <tima1911@yandex.ru>
2025-05-22 14:50:48 +0300

    nix: update rewrite shell.nix file

--- shell.nix
+++ shell.nix
@@ -1,20 +1,7 @@
-let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/24.05";
-  pkgs = import nixpkgs {};
-in
+{ pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
     name = "Chrobelias";
-    packages = [ 
-      # Treesitter-parsers
-      pkgs.vimPlugins.nvim-treesitter-parsers.ocaml 
-      pkgs.vimPlugins.nvim-treesitter-parsers.yaml
-      pkgs.vimPlugins.nvim-treesitter-parsers.jsonc
-      pkgs.vimPlugins.nvim-treesitter-parsers.markdown
-      pkgs.vimPlugins.nvim-treesitter-parsers.markdown_inline
-      pkgs.vimPlugins.nvim-treesitter-parsers.nix
-
-      # Build tools
-      pkgs.opam
+    packages = with pkgs; [ 
+      gmp
     ];
-    shellHook = "eval $(opam env)";
 }
